1f809d3e4b26793404f0829cd268f47bc400f977
Suyash Agrawal <ozym4nd145@outlook.com>
2019-04-09 23:15:23 +0530

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import compression from "compression";  // compresses requests
 import bodyParser from "body-parser";
 import lusca from "lusca";
+import cors from "cors";
 import path from "path";
 import mongoose from "mongoose";
 import bluebird from "bluebird";
@@ -35,6 +36,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.use(
