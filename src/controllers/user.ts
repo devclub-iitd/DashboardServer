31032b4c06569c5daedf12fe003e9f90202014da
Atishya-Jain <atishya.jain@rocketmail.com>
2019-09-07 20:44:28 +0530

    User Update API done

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -242,7 +242,37 @@ const approve_user = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
+const update_record = (req: Request, res: Response, next: NextFunction) => {
+    const my_query = {entry_no: req.params.id};
+    console.log(my_query);
+
+    req.body.query = my_query;
+    if (req.res == undefined) {
+        req.res = res;
+    }
+    if (req.res.locals == undefined) {
+        req.res.locals = {};
+    }
+    req.res.locals.no_send = true;
+    all_query(req, res, next)
+    .then((data: any) => {
+        req.body.query = {};
+        req.params.id = data[0]["_id"];
+        update(req, res, next)
+        .then((fresh_data: any) => {
+            res.json(createResponse("Record updated", fresh_data));
+        })
+        .catch((err: any) => {
+            res.json(createResponse("Error while registering", err));
+        });
+    })
+    .catch((err: any) => {
+        res.json(createResponse("Error while registering", err));
+    });
+};
+
 router.post('/', create);
+router.put('/:id', update_record);
 router.post("/login", login);
 router.post("/approve", approve_user); 		// Add a middleware to checkAdmin
 router.get("/getAll/", all);
