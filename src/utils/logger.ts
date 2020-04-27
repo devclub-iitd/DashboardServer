0dee87dc8ea44b697d953cd7720a7e8d38f5e8d7
Arpit Saxena <arpit.saxena2000@yahoo.in>
2020-04-27 15:47:59 +0530

    Redact passwords when logging requests

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,27 @@
-import winston from "winston";
+import winston, { format } from "winston";
+import traverse from "traverse";
+
+const isSensitive = (key: string | undefined) => {
+  switch (key) {
+    case "password":
+    case "token":
+      return true;
+  }
+  return false;
+}
+
+const removeSensitiveData = format(info => {
+  traverse(info).forEach(function(_) {
+    if (isSensitive(this.key)) this.update("[REDACTED]");
+  })
+  return info;
+});
 
 const logger = winston.createLogger({
-  format: winston.format.json(),
+  format: format.combine(
+    removeSensitiveData(),
+    format.json(),
+  ),
   transports: [
     new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
     new (winston.transports.File)({ filename: "debug.log", level: "debug"})
